e8a1fb4ff8ecd276cbed8553c2b5d819ca239319
JuanZea <vikzea@gmail.com>
2023-09-13 09:21:09 -0500

    wip

--- Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20232/labs1/MainActivity.kt
+++ Lab1-UI/src/main/java/co/edu/udea/compumovil/gr04_20232/labs1/MainActivity.kt
@@ -2,7 +2,10 @@
 
 package co.edu.udea.compumovil.gr04_20232.labs1
 
+import android.app.DatePickerDialog
+import android.icu.util.Calendar
 import android.os.Bundle
+import android.widget.DatePicker
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -10,6 +13,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -19,6 +23,8 @@ import androidx.compose.foundation.selection.selectable
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Face
 import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -36,6 +42,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.TextStyle
@@ -45,6 +52,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import co.edu.udea.compumovil.gr04_20232.labs1.ui.theme.Labs20232Gr04Theme
+import java.util.Date
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -62,7 +70,7 @@ fun app() {
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .background(Color.Gray)
+            .background(Color.White)
     ) {
         Text(
             text = "Información Personal",
@@ -138,11 +146,58 @@ fun app() {
 
             )
             Text(text="Sexo")
+            var sex by remember { mutableStateOf("female") }
 
+            
+            RadioButton(
+                selected = sex === "female",
+                onClick = { sex = "female" }
+            )
+            Text(text="Femenino")
+            
+            RadioButton(
+                selected = sex === "male",
+                onClick = { sex = "male" }
+            )
+            Text(text="Masculino")
+        }
+
+        Column(modifier = Modifier.fillMaxSize(), verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally){
+    val mContext = LocalContext.current
+
+    val mYear: Int
+    val mMonth: Int
+    val mDay: Int
+
+    val mCalendar = Calendar.getInstance()
+
+    mYear = mCalendar.get(Calendar.YEAR)
+    mMonth = mCalendar.get(Calendar.MONTH)
+    mDay = mCalendar.get(Calendar.DAY_OF_MONTH)
+    mCalendar.time = Date()
+
+    val mDate = remember { mutableStateOf("") }
+    val mDatePickerDialog = DatePickerDialog(
+        mContext,
+        { _: DatePicker, mYear: Int, mMonth: Int, mDayOfMonth: Int ->
+            mDate.value = "$mDayOfMonth/${mMonth+1}/$mYear"
+        }, mYear, mMonth, mDay
+    )
+
+            Button(onClick = {
+            mDatePickerDialog.show()
+        }, colors = ButtonDefaults.buttonColors(containerColor = Color.Yellow) ) {
+            Text(text = "Date Picker", color = Color.Black)
+        }
+
+        // Adding a space of 100dp height
+        Spacer(modifier = Modifier.size(70.dp))
+
+        // Displaying the mDate value in the Text
+        Text(text = "Fecha: ${mDate.value}", fontSize = 25.sp, textAlign = TextAlign.Center)
         }
     }
 }
-
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
